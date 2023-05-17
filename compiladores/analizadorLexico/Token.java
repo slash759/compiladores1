4641302181f4cd085193e0e12468949807ed39ee
slash759 <78600263+slash759@users.noreply.github.com>
2023-05-17 08:56:43 -0500

    Add files via upload

---
+++ compiladores/analizadorLexico/Token.java
@@ -0,0 +1,13 @@
+package analizadorLexico;
+
+public class Token {
+  public final int etiqueta;
+
+  public Token(int t) {
+    etiqueta = t;
+  }
+
+  public String toString() {
+    return "" + (char) etiqueta;
+  }
+}
